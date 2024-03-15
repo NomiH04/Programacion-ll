0ea9df792a0c2dafa4ce536b6ccbbf42653c103d
NomiH <Usuario@SPLab1-15>
2024-03-15 08:36:03 -0600

    primera carga

---
+++ src/main/java/ucr/ac/cr/programacionll/Programacionll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacionll;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Programacionll {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
