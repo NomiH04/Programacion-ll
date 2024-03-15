4804a00bf78dcc3e8e8b94292a6bce0c6f1af519
NomiH <Usuario@SPLab1-15>
2024-03-15 08:40:40 -0600

    Se cambio la pendejada de hola mundo

--- src/main/java/ucr/ac/cr/programacionll/Programacionll.java
+++ src/main/java/ucr/ac/cr/programacionll/Programacionll.java
@@ -11,6 +11,6 @@ package ucr.ac.cr.programacionll;
 public class Programacionll {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hola mundo!");
     }
 }
